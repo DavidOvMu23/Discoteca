226e49514cb84da83a9eadb4d97e5c9c2ec131da
David Oviedo <david23oviedo@gmail.com>
2026-02-10 11:17:38 +0100

    Vistas Principales hechas

--- View/usuarios.xaml.cs
+++ View/usuarios.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace View
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para reservas.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class usuarios : Window
     {
-        public MainWindow()
+        public usuarios()
         {
             InitializeComponent();
+        }
+
+        private void button_discos1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ View/vinilos.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Lógica de interacción para reservas.xaml
+    /// </summary>
+    public partial class vinilos : Window
+    {
+        public vinilos()
+        {
+            InitializeComponent();
+        }
+
+        private void button_discos1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
